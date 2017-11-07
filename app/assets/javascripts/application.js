2236544dba8233d3ba3657f7a3f8a7ae809e93e9
Titi <thibault.buathier@k-net.pro>
2017-11-07 15:47:52 +0100

    column pictures

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,16 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+function getRandomSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+var allImages = "";
+
+for (var i = 0; i < 25; i++) {
+  var width = getRandomSize(200, 400);
+  var height =  getRandomSize(200, 400);
+  allImages += '<div class="container-picture"><div class="tag">Truc</div><img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty"></div>';
+}
+
+$('#photos').append(allImages);
